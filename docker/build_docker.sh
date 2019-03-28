db0347a39b35088b452365a63e45f460896c8b86
SoonminHwang <jjang9hsm@gmail.com>
2019-03-28 14:38:20 +0900

    pytorch-1.0, cuda 9.0

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,6 +1,6 @@
 USERNAME=$(whoami)
 sudo nvidia-docker build -t \
-	${USERNAME}/pytorch-1.0:cuda10.0-cudnn7-dev-ubuntu16.04 \
+	${USERNAME}/pytorch-1.0:cuda9.0-cudnn7-dev-ubuntu16.04 \
 	--build-arg UID=$(id -u) \
 	--build-arg USER_NAME=${USERNAME} \
 	.
